285c185d8b926138315e4ca85b060c5e50b2be92
Velma <akinyivee10@gmail.com>
2024-02-28 09:04:27 +0300

    set up user api

---
+++ lib/github_user_search/users_api.ex
@@ -0,0 +1,15 @@
+defmodule GithubUserSearch.UsersAPI do
+  @moduledoc false
+
+  @callback fetch_user(binary()) :: {:ok, map()} | {:error, binary()}
+
+  def fetch_user(username), do: impl().fetch_user(username)
+
+  defp impl do
+    Application.get_env(
+      :github_user_search,
+      :users_api_client_module,
+      GithubUserSearch.ExternalUsersAPI
+    )
+  end
+end
