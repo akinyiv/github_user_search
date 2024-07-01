3972a7be61e5063745c32c79e4777ad0ee6a140f
Velma <akinyivee10@gmail.com>
2024-07-01 13:43:36 +0300

    mock test on client

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,19 +14,6 @@
 //
 //     import "some-package"
 //
-function darkExpected() {
-    return localStorage.theme === 'dark' || (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches);
-  }
-  
-function initDarkMode() {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (darkExpected()) 
-        document.documentElement.classList.add('dark');  
-    else
-        document.documentElement.classList.remove('dark');
-    
-}
   
 window.addEventListener("toggle-theme", () => {
     if (localStorage.theme === "dark") {
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
+Mox.defmock(GithubUserSearch.ClientMock, for: GithubUserSearch.Client)
+Application.put_env(:github_user_search, :github_user_search_client, GithubUserSearch.ClientMock)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(GithubUserSearch.Repo, :manual)
